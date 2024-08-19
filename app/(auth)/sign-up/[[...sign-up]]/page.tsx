4b85c2bfa704ab4382027901ece57f721fa13f86
racquellgrey <racquellg@vt.edu>
2024-08-19 10:39:26 -0400

    first commit

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,7 @@
+import { SignIn, SignUp } from "@clerk/nextjs";
+
+export default function Page() {
+    return(
+            <SignUp />       
+    )
+}
---
+++ components/shared/Header.tsx
@@ -0,0 +1,50 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import {
+  ClerkProvider,
+  SignIn,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
+import { Button } from '../ui/button'
+import NavItems from './NavItems'
+import MobileNav from './MobileNav'
+
+
+const Header = () => {
+  return (
+    <header className='w-full border-b'>
+        <div className="wrapper flex items-center justify-between">
+            <Link href="/" className="w-36">
+                <Image src="/assets/images/logo2.svg" width={128} height={38} 
+                alt="Tessera logo" />
+            </Link>
+
+          <SignedIn>
+            <nav className="md:flex-between hidden w-full max-w-xs">
+            <NavItems />
+            </nav>
+          </SignedIn>
+
+           <div className="flex w-32 justify-end gap-3">
+           <SignedIn>
+            <UserButton userProfileUrl="/"/>
+            <MobileNav />
+           </SignedIn>
+            <SignedOut>
+              <Button asChild className="rounded-full" size="lg">
+                <Link href="/sign-in">
+                    Login
+                </Link>
+              </Button>
+            </SignedOut>
+           </div>
+        </div>
+    </header>
+  )
+}
+
+export default Header
