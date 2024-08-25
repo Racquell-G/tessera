25473c6596761575a852aea36b3f63c2c32ade88
Racquell <racquellgrey@gmail.com>
2024-08-25 14:26:20 -0400

    Reclone after connection issues

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## Overview 
-A powerful web application for organizing and managing events of all sizes. Whether you're hosting virtual conference, summit, concert, workshop, or community gathering, tessera provides an intuitive interface and robust features to ensure smooth and successful event management.
+A powerful web application for organizing and managing events of all sizes. Whether you're hosting a virtual conference, summit, concert, workshop, or community gathering, tessera provides an intuitive interface and robust features to ensure smooth and successful event management.
 
 ## Key Features (so far):
 Event Creation and Management: Easily create and manage multiple events with customizable options for dates, locations, event details, pictures, and links.
@@ -16,3 +16,12 @@ Seat selection: The ability to choose where you want to sit in a reserved seatin
 Customizable Event Pages: Design and personalize event pages with rich content, including images, descriptions, and schedules.
 
 Email Notifications: Automated email notifications for ticket confirmations, reminders, and updates.
+
+## Work Log (5 hours each day):
+Day 1: Learning more about Next.js, Typescript, Clerk, Mongoose, Vercel, ShadCn and much more. Lots and lots of wireframing. And finally icon and images creation.
+
+Day 2: Working on the homepage and users profiles in Clerk. Lots of debugging.
+
+Day 3: Working on setting up the backend with Mongoose as well as Deploying with Vercel. Hooray!!
+
+Day 4: Working on the "Create Event" page as well as the "Final Summary" (and hopefully the purchase ticket page). Lots and lots of debugging this day.
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,32 +1,59 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
-import Link from "next/link";
+import Collection from '@/components/shared/Collection'
+import { Button } from '@/components/ui/button'
+import { getAllEvents } from '@/lib/actions/event.actions';
+import { SearchParamProps } from '@/types';
+import Image from 'next/image'
+import Link from 'next/link'
 
+export default async function Home({ searchParams }: SearchParamProps) {
+  const page = Number(searchParams?.page) || 1;
+  const searchText = (searchParams?.query as string) || '';
+  const category = (searchParams?.category as string) || '';
 
-export default function Home() {
+  const events = await getAllEvents({
+    query: searchText,
+    category,
+    page,
+    limit: 6
+  })
+
   return (
-   <>
-    <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
-      <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
-        <div className="flex flex-col justify-center gap-8">
-          <h1 className="h1-bold">Your Next Great Event Starts Here!</h1>
-          <p className="p-regular-20 md:p-regular-24">Seamlessly create, manage, and experience unforgettable moments with our all-in-one event hosting platform.</p>
-          <Button size="lg" asChild className="button w-full sm:w-fit">
-            <Link href="#events">Explore Now</Link>
-          </Button>
+    <>
+      <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
+        <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
+          <div className="flex flex-col justify-center gap-8">
+            <h1 className="h1-bold">Host, Connect, Celebrate: Your Events, Our Platform!</h1>
+            <p className="p-regular-20 md:p-regular-24">Book and learn helpful tips from 3,168+ mentors in world-class companies with our global community.</p>
+            <Button size="lg" asChild className="button w-full sm:w-fit">
+              <Link href="#events">
+                Explore Now
+              </Link>
+            </Button>
+          </div>
+
+          <Image 
+            src="/assets/images/hero.png"
+            alt="hero"
+            width={1000}
+            height={1000}
+            className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"
+          />
         </div>
-        <Image 
-          src="/assets/images/hero.png" alt="hero" width={1000} height={1000} className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"
+      </section> 
+
+      <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
+        <h2 className="h2-bold">Thousands of<br /> Events to Choose From</h2>
+
+        <Collection 
+          data={events?.data}
+          emptyTitle="No Events Found"
+          emptyStateSubtext="Come back later"
+          collectionType="All_Events"
+          limit={6}
+          page={page}
+          totalPages={events?.totalPages}
         />
-      </div>
-    </section>
-    <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
-      <h2 className="h2-bold">Trusted by <br/>Thousands of Events</h2>
-      <div className="flex w-full flex-col gap-5 md:flex-row">
-        Search
-        CategoryFilter
-      </div>
-    </section>
+      </section>
     </>
   )
 }
