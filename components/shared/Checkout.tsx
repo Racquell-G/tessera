93e43b7c2421f6b80be250ead4ce85ec7cd23a86
Racquell <racquellg@vt.edu>
2024-09-08 19:20:49 -0400

    Update Checkout.tsx

--- components/shared/Checkout.tsx
+++ components/shared/Checkout.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react'
 import { loadStripe } from '@stripe/stripe-js';
-
 import { IEvent } from '@/lib/database/models/event.model';
 import { Button } from '../ui/button';
 import { checkoutOrder } from '@/lib/actions/order.actions';
@@ -42,4 +41,3 @@ const Checkout = ({ event, userId }: { event: IEvent, userId: string }) => {
 }
 
 export default Checkout
-
